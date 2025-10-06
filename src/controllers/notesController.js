c6ab49a42ae04e39a65292aa4f2faa6da2bba6c1
Yaroslav <yaroslavlit@gmail.com>
2025-10-06 22:08:56 +0200

    change text and fix error handler

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -11,7 +11,7 @@ export const getNoteById = async (req, res) => {
   const note = await Note.findById(noteId);
 
   if (!note) {
-    throw createHttpError(404, 'Student not found');
+    throw createHttpError(404, 'Note not found');
   }
 
   res.status(200).json(note);
